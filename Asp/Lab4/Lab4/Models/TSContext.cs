01b73629925d76ebbe8349b1c14c54f295709d3d
Dmitriy <dyrda.dmitrij@mail.ru>
2020-09-14 23:31:23 +0300

    3

---
+++ Asp/Lab4/Lab4/Models/TSContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Lab4.Models
+{
+    public class TSContext:DbContext
+    {
+        public DbSet<TS> TSses { get; set; }
+    }
+}
